dfda3eff4cc508dcf6bbc54ec5f4b22aa6e2f301
mahdi0011 <gooddevil1062@gmail.com>
2016-06-04 01:29:34 -0700

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -263,27 +263,10 @@ function create_config( )
     },
     sudo_users = {24709501},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[MeGa shield v2.9
-An advanced administration bot based on TG-CLI written in Lua
-
-Github:
-https://github.com/hafez16/mega-shield
-
-Admins:
-@hafez1116hafez [Developer]
-@amirho3ien911 [Developer]
-@Eblis_alone [Manager]
-@Mmd_hei [sponsor]
-
-Special thanks to
-SEEDTEAM
-Hextor team
-Magic team
-Avira team
-
-Our channels
-@shieldTM [persian]
-]],
+    about_text = [[
+    DJMehdi Bot
+    Version 2.0.2
+    Enjoy Friend!]],
     help_text_realm = [[
 Realm Commands:
 

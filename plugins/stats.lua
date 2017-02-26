dc2fa013bf20e801aa15a1c416bd277baffd2fc7
mahdi0011 <gooddevil1062@gmail.com>
2017-02-26 15:23:41 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -144,7 +144,7 @@ return {
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
     "^[#!/]([Ss]tats) (shield)",
-	"^[#!/]([Ss]hield)"
+	"^[#!/]([Cc]reator)"
     }, 
   run = run
 }

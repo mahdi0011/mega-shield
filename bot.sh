262351f418cb57d502f655e7968937ccad3005ee
mahdi0011 <gooddevil1062@gmail.com>
2017-04-05 17:22:11 +0430

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
 sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
+ tar zxpf luarocks-2.2.2.tar.gz
+ cd luarocks-2.2.2
+ ./configure; sudo make bootstrap
+ sudo luarocks install luasocket
+ sudo luarocks install luasec
+ sudo luarocks install redis-lua
+ sudo luarocks install lua-term
+ sudo luarocks install serpent
+ sudo luarocks install dkjson
+ sudo luarocks install lanes
+ sudo luarocks install Lua-cURL
+ sudo luarocks install luaxmlrpc
+ cd ..
+ sudo apt-get install curl

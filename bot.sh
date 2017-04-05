497cf3d20336b43ac3c3d63d1e269db499511360
mahdi0011 <gooddevil1062@gmail.com>
2017-04-05 15:36:54 +0430

    Create bot.sh

---
+++ bot.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev tmux subversion
